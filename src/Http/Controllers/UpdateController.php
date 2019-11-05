248df5d2fadbad5c30c7565984fa9bef53537a20
Martin M <skydiver@users.noreply.github.com>
2019-11-04 23:06:04 -0300

    cache latest Laravel version

--- src/Http/Controllers/UpdateController.php
+++ src/Http/Controllers/UpdateController.php
@@ -2,6 +2,7 @@
 
 namespace Beyondcode\LaravelUpdateCard\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Packagist\Api\Client;
 use Beyondcode\LaravelUpdateCard\Version;
 
@@ -9,13 +10,14 @@ class UpdateController
 {
     public function check()
     {
-        $client = new Client();
 
-        $package = $client->get('laravel/laravel');
-
-        $versions = array_map(function ($version) {
-            return $version->getVersion();
-        }, $package->getVersions());
+        $versions = Cache::remember('laravel-update-card', 3600, function () {
+            $client = new Client();
+            $package = $client->get('laravel/laravel');
+            return array_map(function ($version) {
+                return $version->getVersion();
+            }, $package->getVersions());
+        });
 
         $current = app()->version();
         $latest = Version::latest($versions);
